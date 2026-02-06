73c726fc942a4a34683f54301bb95ba271e2b3ba
admaneamina23-prog <admaneamina23@gmail.com>
2026-02-06 15:26:57 +0100

    modif 2

--- src/main/java/tn/m104/rh/SchoolApplication.java
+++ src/main/java/tn/m104/rh/SchoolApplication.java
@@ -9,7 +9,7 @@ public class SchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
-	}
+	}  // 1. Créer une classe principale pour votre application Spring Boot, par exemple `SchoolApplication.java`, et ajoutez l'annotation `@SpringBootApplication` pour indiquer que c'est une application Spring Boot.
 
 }
 	
